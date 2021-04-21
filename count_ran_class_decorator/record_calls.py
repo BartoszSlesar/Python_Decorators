4d3a2fc9ed0e43d65676dc95d73186d0b6530f96
Bartosz Slesar <bartoszslesar@gmail.com>
2021-04-21 21:40:08 +0200

    extend record_class with wrapt.ObjectProxy  and calls_record classes with object

--- count_ran_class_decorator/record_calls.py
+++ count_ran_class_decorator/record_calls.py
@@ -1,17 +1,34 @@
+from functools import update_wrapper
+
+import wrapt as wrapt
+
+
 # noinspection PyPep8Naming
-class record_calls(object):
-    """ decorator counts how many time passed_funtion ran"""
+class record_calls(wrapt.ObjectProxy):
+    """this decorator counts how many time passed_funtion ran"""
 
     def __init__(self, passed_function):
-        self.passed_function = passed_function
+        super().__init__(passed_function)
         self.call_count = 0
 
     def __call__(self, *args, **kwargs):
         self.call_count += 1
+        return self.__wrapped__(*args, **kwargs)
+
+
+
+
+
+
+# noinspection PyPep8Naming
+class calls_record(object):
+    """this decorator counts how many time passed_funtion ran"""
+
+    def __init__(self, passed_function):
+        self.passed_function = passed_function
+        self.call_count = 0
+        update_wrapper(self, passed_function)
+
+    def __call__(self, *args, **kwargs):
+        self.call_count += 1
         return self.passed_function(*args, **kwargs)
-
-
-@record_calls
-def greet(name="world"):
-    """Greet someone by their name."""
-    print(f"Hello {name}")
